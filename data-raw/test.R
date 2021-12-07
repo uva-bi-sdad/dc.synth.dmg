4e47b1fa5d845b152552ced0e3e83c19a867880a
Aaron Schroeder <dads2busy@gmail.com>
2021-12-07 17:31:05 -0500

    get all capital region

--- data-raw/test.R
+++ data-raw/test.R
@@ -1,8 +1,37 @@
 library(dc.synth.dmg)
+# DC Region County FIPS Codes
+cnty_fips <- list(
+  c("VA", "059"), # Fairfax
+  c("VA", "600"), # Fairfax City
+  c("VA", "013"), # Arlington
+  c("VA", "107"), # Loudoun
+  c("VA", "510"), # Alexandria
+  c("VA", "683"), # Manassas
+  c("VA", "610"), # Falls Church
+  c("DC", "001"), # DC
+  c("MD", "031"), # Montgomery
+  c("MD", "033") # Prince Georges
+)
+
 # Get ACS and Block Address Count Data
-mydata <- get_data(state_abbrev = "VA", county_fips = "013")
+if (exists("dt_out")) rm(dt_out)
+for (i in 1:length(cnty_fips)) {
+  mydata <- get_data(state_abbrev = cnty_fips[[i]][1], county_fips = cnty_fips[[i]][2])
+  bk_dmgs <- generate_block_dmgs(acs_data = mydata[[1]], bac_data = mydata[[2]])
+  if(exists("dt_out")) {
+    dt_out <- data.table::rbindlist(list(dt_out, bk_dmgs))
+  } else {
+    dt_out <- bk_dmgs
+  }
+}
+
+
 # Generate Demographic Estimates per Census Block
-bk_dmgs <- generate_block_dmgs(acs_data = mydata$acs_data_51013, bac_data = mydata$bac_data_51013)
+bk_dmgs <- generate_block_dmgs(acs_data = mydata$acs_data_11001, bac_data = mydata$bac_data_11001)
+
+
+
+
 # Load the Arlington VA Master Housing Unit Database
 va_arl_housing_unit_db <- sf::st_read("https://opendata.arcgis.com/datasets/628f6de7205641169273ea684a74fb0f_0.geojson")
 
